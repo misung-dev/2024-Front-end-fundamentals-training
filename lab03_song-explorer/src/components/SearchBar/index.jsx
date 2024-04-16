4a2fc4c8b66d4aa5cf739ad9f9f11b307142c885
misung.dev <misung.dev@gmail.com>
2024-04-16 21:18:59 +0900

    Feat: 엔터 쳤을 때 검색되도록

--- lab03_song-explorer/src/components/SearchBar/index.jsx
+++ lab03_song-explorer/src/components/SearchBar/index.jsx
@@ -5,6 +5,12 @@ import axios from "axios";
 export default function SearchBar({ setSongList }) {
 	const inputRef = useRef();
 
+	const handleEnter = (e) => {
+		if (e.key == "Enter") {
+			handleSearch();
+		}
+	};
+
 	const handleSearch = () => {
 		axios.get(`https://api.manana.kr/karaoke/song/${inputRef.current.value}.json`).then((res) => {
 			setSongList(res.data);
@@ -13,7 +19,12 @@ export default function SearchBar({ setSongList }) {
 
 	return (
 		<div className="SearchBar">
-			<input type="text" className="SearchBar__input" ref={inputRef} />
+			<input
+				type="text"
+				className="SearchBar__input"
+				ref={inputRef}
+				onKeyDown={(e) => handleEnter(e)}
+			/>
 			<button className="SearchBar__button" onClick={handleSearch}>
 				검색
 			</button>
